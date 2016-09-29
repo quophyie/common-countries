399e54d0b3ef9c828242358c223e7cee139be651
Christos Ziogas <ziogasc@gmail.com>
2016-09-29 13:44:57 +0100

    Initial commint

---
+++ index.js
@@ -0,0 +1,13 @@
+var countries = require('./countries.json');
+var sortedCountries = Object.keys(countries).map(function (ccode) {
+  return {
+    ccode: ccode,
+    cname: countries[ccode]
+  }
+}).sort(function (a, b) {
+  if ( a.cname < b.cname ) return -1
+  if ( a.cname > b.cname ) return 1
+  return 0
+})
+
+exports.sortedCountries = sortedCountries;
